9385eca446f2bb976881f0e8eec716ac5962fc6c
Alnk-李成果 <1029612787@qq.com>
2024-11-30 12:22:08 +0800

    if判断语句

--- Go基础知识/06-if条件判断语句/main.go
+++ Go基础知识/06-if条件判断语句/main.go
@@ -5,22 +5,22 @@ import "fmt"
 // if判断
 
 func main() {
-	// age := 18
-	// if age > 18 {
-	// 	fmt.Println("aaa")
-	// }else {
-	// 	fmt.Println("bbb")
-	// }
+	age := 18
+	if age > 18 {
+		fmt.Println("aaa")
+	} else {
+		fmt.Println("bbb")
+	}
 
 	// 多个判断条件
-	// age := 19
-	// if age > 35 {
-	// 	fmt.Println("a")
-	// }else if age >18 {
-	// 	fmt.Println("b")
-	// }else {
-	// 	fmt.Println("c")
-	// }
+	age = 19
+	if age > 35 {
+		fmt.Println("a")
+	} else if age > 18 {
+		fmt.Println("b")
+	} else {
+		fmt.Println("c")
+	}
 
 	// 作用域
 	// age变量此时只在if条件判断语句中生效
@@ -29,5 +29,4 @@ func main() {
 	} else {
 		fmt.Println("b")
 	}
-
 }
