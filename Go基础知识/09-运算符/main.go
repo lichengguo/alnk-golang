86fb24a28dd7995bd86d385a1b36fc09a0df532e
Alnk-李成果 <1029612787@qq.com>
2024-11-30 17:05:12 +0800

    运算符

--- Go基础知识/09-运算符/main.go
+++ Go基础知识/09-运算符/main.go
@@ -1,8 +1,7 @@
+// 运算符
 package main
 
 import "fmt"
-
-// 运算符
 
 func main() {
 	var (
@@ -11,35 +10,39 @@ func main() {
 	)
 
 	// 算术运算符
-	fmt.Println(a + b)
-	fmt.Println(a - b)
-	fmt.Println(a * b)
-	fmt.Println(a / b)
-	fmt.Println(a % b)
-	a++ // 单独的语句 --> a=a+1
-	b-- // 单独的语句 --> b=b-1
-	fmt.Println(a, b)
+	fmt.Println("--------------- 算术运算符 ---------------------")
+	fmt.Println(a + b) // 7
+	fmt.Println(a - b) // 3
+	fmt.Println(a * b) // 10
+	fmt.Println(a / b) // 2
+	fmt.Println(a % b) // 1
+	a++                // 单独的语句 --> a=a+1
+	b--                // 单独的语句 --> b=b-1
+	fmt.Println(a, b)  // 6 1
 
 	// 关系运算符
 	// Go语言是强类型语言，相同类型的变量才能比较
-	fmt.Println(a == b)
-	fmt.Println(a != b)
-	fmt.Println(a >= b)
-	fmt.Println(a <= b)
-	fmt.Println(a > b)
-	fmt.Println(a < b)
+	fmt.Println("--------------- 关系运算符 ---------------------")
+	fmt.Println(a == b) // false
+	fmt.Println(a != b) // true
+	fmt.Println(a >= b) // true
+	fmt.Println(a <= b) // false
+	fmt.Println(a > b)  // true
+	fmt.Println(a < b)  // false
 
 	// 字符也可以比较
+	fmt.Println("--------------- 字符 ---------------------")
 	c1 := '中'
 	c2 := '国'
-	fmt.Printf("==%c\n", c1)    // 中
-	fmt.Println("===", c1)      // 20013
-	fmt.Println("===", c2)      // 22269
-	fmt.Println("===", c1 < c2) // 底层是int32类型，所以可以比较
-	fmt.Printf("%T\n", c1)      // int32
+	fmt.Printf("c1: %c\n", c1) // 中
+	fmt.Println("c1: ", c1)    // 20013
+	fmt.Println("c2: ", c2)    // 22269
+	fmt.Println(c1 < c2)       // true 底层是int32类型，所以可以比较
+	fmt.Printf("c1: %T\n", c1) // int32
 
 	// 逻辑运算符
 	// 如果年龄大于18岁 并且 并且小于60岁
+	fmt.Println("--------------- 逻辑运算符 ---------------------")
 	age := 22
 	if age > 18 && age < 60 {
 		fmt.Println("上班族")
@@ -62,22 +65,23 @@ func main() {
 	// 位运算：针对的是二进制数
 	// 5的二进制表示：0101
 	// 2的二进制表示：0010
-
 	// &:按位与(两位均为1才为1)
-	fmt.Println(5 & 2)
+	fmt.Println("--------------- 位运算 ---------------------")
+	fmt.Println(5 & 2) // 0
 	// |:按位或（两位有一个为1就为1）
-	fmt.Println(5 | 2)
+	fmt.Println(5 | 2) // 7
 	// ^:按位异或（两位不一样则为1）
-	fmt.Println(5 ^ 2)
+	fmt.Println(5 ^ 2) // 7
 	// <<:将二进制位左移指定位数
 	fmt.Println(5 << 1)  // 1010 => 10
 	fmt.Println(1 << 10) // 10000000000 => 1024
 	// >>:将二进制位右移指定的位数
-	fmt.Println(5 >> 2)
+	fmt.Println(5 >> 2) // 1
 	// var m = int8(1)      // 只能存8位
 	// fmt.Println(m << 10) // 因为int8 只能存储8位，向左移10位的话，就位0了
 
 	// 赋值运算符，用来给变量赋值的
+	fmt.Println("--------------- 赋值运算符 ---------------------")
 	var x int
 	x = 10
 	x += 1 // x = x + 1
