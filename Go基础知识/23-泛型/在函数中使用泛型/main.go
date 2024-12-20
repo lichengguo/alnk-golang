76d57c2f64225104255fe524339216d204c45904
Alnk <1029612787@qq.com>
2024-12-20 13:00:25 +0800

    泛型

--- Go基础知识/23-泛型/在函数中使用泛型/main.go
+++ Go基础知识/23-泛型/在函数中使用泛型/main.go
@@ -1,21 +1,24 @@
 package main
 
+// 在函数中使用泛型约束
+
 import "fmt"
 
 type Constraint interface {
+    // 只允许以下的几种数据类型传参
     int | float64 | string // | []int
 }
 
 func Printer[T Constraint](vals ...T) {
     for _, v := range vals {
-        fmt.Printf("%v ", v)
+        fmt.Printf("%v \n", v)
     }
     fmt.Println()
 }
 
 func main() {
     Printer(1, 2, 3)
-    Printer(1.1, 2.2, 3.3)
+    Printer(1.1, 2.2, 3.3, 4.4)
     Printer("a", "b", "c")
     // Printer([]int{1,2}, []int{5, 6}, []int{8, 9})
 }
